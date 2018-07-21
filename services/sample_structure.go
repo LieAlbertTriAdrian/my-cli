d5da2f45145f70be80fe518a034b03f70401c599
Albert Lie <alberttri23@gmail.com>
2018-07-21 21:06:30 +0700

    add aws sdk

--- services/sample_structure.go
+++ services/sample_structure.go
@@ -1,4 +1,4 @@
-package aws
+package sample_structure
 
 import (
 	"fmt"
@@ -6,6 +6,22 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"	
  	"github.com/aws/aws-sdk-go/service/ec2"
 )
+
+type GetAWSEC2InstanceRequest struct {
+
+}
+
+type GetAWSEC2InstanceResponse struct {
+
+}
+
+func CreateGetAWSEC2InstanceRequest() {
+	fmt.Println("Create GET AWS EC2 Instance Request")
+}
+
+func CreateGetAWSEC2InstanceResponse() () {
+	fmt.Println("Create GET AWS EC2 Instance Response")	
+}
 
 func GetAWSEC2InstanceInformation(RandomString string) string {
 	// AWSTry()
