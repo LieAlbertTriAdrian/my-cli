ab6c91f07c2347d9656466aa2fde514e9b445065
Albert Lie <alberttri23@gmail.com>
2018-07-21 20:51:21 +0700

    initialize structure foldering

--- main.go
+++ main.go
@@ -1,105 +1,110 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
+	// "encoding/json"
+	// "errors"
 	"fmt"
-	"net/http"
-	"os"
-	"strings"
-	"time"
+	// "net/http"
+	// "os"
+	// "strings"
+	// "time"
 
-	"github.com/manifoldco/promptui"
+	"github.com/LieAlbertTriAdrian/my-cli/services/aws"
+	// "github.com/manifoldco/promptui"
 )
 
 func main() {
-	prompt := promptui.Prompt{
-		Label: "Search",
-		Validate: func(input string) error {
-			if len(input) < 3 {
-				return errors.New("Search term must have at least 3 characters")
-			}
-			return nil
-		},
-	}
 
-	keyword, err := prompt.Run()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	result := aws.GetAWSEC2InstanceInformation("my information")
 
-	const apiURL = "http://www.recipepuppy.com/api/"
+	fmt.Println(result)
+// 	prompt := promptui.Prompt{
+// 		Label: "Search",
+// 		Validate: func(input string) error {
+// 			if len(input) < 3 {
+// 				return errors.New("Search term must have at least 3 characters")
+// 			}
+// 			return nil
+// 		},
+// 	}
 
-	query := apiURL + "?q=" + keyword
+// 	keyword, err := prompt.Run()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
 
-	client := &http.Client{Timeout: 10 * time.Second}
+// 	const apiURL = "http://www.recipepuppy.com/api/"
 
-	r, err := client.Get(query)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer r.Body.Close()
+// 	query := apiURL + "?q=" + keyword
 
-	type recipe struct {
-		Title       string
-		Href        string
-		Ingredients string
-	}
+// 	client := &http.Client{Timeout: 10 * time.Second}
 
-	type apiResponse struct {
-		Results []recipe
-	}
+// 	r, err := client.Get(query)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+// 	defer r.Body.Close()
 
-	var res apiResponse
+// 	type recipe struct {
+// 		Title       string
+// 		Href        string
+// 		Ingredients string
+// 	}
 
-	err = json.NewDecoder(r.Body).Decode(&res)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+// 	type apiResponse struct {
+// 		Results []recipe
+// 	}
 
-	funcMap := promptui.FuncMap
-	funcMap["truncate"] = func(size int, input string) string {
-		if len(input) <= size {
-			return input
-		}
-		return input[:size-3] + "..."
-	}
+// 	var res apiResponse
 
-	templates := promptui.SelectTemplates{
-		Active:   `🍕 {{ .Title | cyan | bold }}`,
-		Inactive: `   {{ .Title | cyan }}`,
-		Selected: `{{ "✔" | green | bold }} {{ "Recipe" | bold }}: {{ .Title | cyan }}`,
-		Details: `
-Ingredients:
-{{ .Ingredients | truncate 80 }}`,
-	}
+// 	err = json.NewDecoder(r.Body).Decode(&res)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
 
-	list := promptui.Select{
-		Label:     "Recipe",
-		Items:     res.Results,
-		Templates: &templates,
-		Searcher: func(input string, idx int) bool {
-			recipe := res.Results[idx]
-			title := strings.ToLower(recipe.Title)
+// 	funcMap := promptui.FuncMap
+// 	funcMap["truncate"] = func(size int, input string) string {
+// 		if len(input) <= size {
+// 			return input
+// 		}
+// 		return input[:size-3] + "..."
+// 	}
 
-			if strings.Contains(title, input) {
-				return true
-			}
-			if strings.Contains(recipe.Ingredients, input) {
-				return true
-			}
-			return false
-		},
-	}
+// 	templates := promptui.SelectTemplates{
+// 		Active:   `🍕 {{ .Title | cyan | bold }}`,
+// 		Inactive: `   {{ .Title | cyan }}`,
+// 		Selected: `{{ "✔" | green | bold }} {{ "Recipe" | bold }}: {{ .Title | cyan }}`,
+// 		Details: `
+// Ingredients:
+// {{ .Ingredients | truncate 80 }}`,
+// 	}
 
-	idx, _, err := list.Run()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+// 	list := promptui.Select{
+// 		Label:     "Recipe",
+// 		Items:     res.Results,
+// 		Templates: &templates,
+// 		Searcher: func(input string, idx int) bool {
+// 			recipe := res.Results[idx]
+// 			title := strings.ToLower(recipe.Title)
 
-	fmt.Println(res.Results[idx].Href)
+// 			if strings.Contains(title, input) {
+// 				return true
+// 			}
+// 			if strings.Contains(recipe.Ingredients, input) {
+// 				return true
+// 			}
+// 			return false
+// 		},
+// 	}
+
+// 	idx, _, err := list.Run()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		os.Exit(1)
+// 	}
+
+// 	fmt.Println(res.Results[idx].Href)
 }
---
+++ services/aws/main.go
@@ -0,0 +1,26 @@
+package aws
+
+import (
+	"fmt"
+)
+
+type GetAWSEC2InstanceRequest struct {
+
+}
+
+type GetAWSEC2InstanceResponse struct {
+
+}
+
+func CreateGetAWSEC2InstanceRequest() {
+	fmt.Println("Create GET AWS EC2 Instance Request")
+}
+
+func CreateGetAWSEC2InstanceResponse() () {
+	fmt.Println("Create GET AWS EC2 Instance Response")	
+}
+
+func GetAWSEC2InstanceInformation(RandomString string) string {
+
+	return RandomString
+}
